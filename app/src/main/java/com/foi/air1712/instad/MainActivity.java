38a1eb806a2f5df5375de450fec9b8e6ef9dbe80
hranicd <dhranic2@foi.hr>
2017-10-29 10:13:42 +0100

    empty project

---
+++ app/src/main/java/com/foi/air1712/instad/MainActivity.java
@@ -0,0 +1,13 @@
+package com.foi.air1712.instad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
