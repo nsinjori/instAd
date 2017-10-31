8adc8b6f3f077d50aad0fd9cbe74f42757cd3e01
hranicd <dhranic2@foi.hr>
2017-10-31 01:20:22 +0100

    signup UI
    SignUpActivity added and UI created

---
+++ app/src/main/java/com/foi/air1712/instad/accountManagement/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.foi.air1712.instad.accountManagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.foi.air1712.instad.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
