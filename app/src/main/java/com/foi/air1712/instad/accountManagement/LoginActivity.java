93916d760f82d6865663518bbaeca382ee659a09
hranicd <dhranic2@foi.hr>
2017-10-31 15:34:01 +0100

    skip login
    show event info without login

--- app/src/main/java/com/foi/air1712/instad/accountManagement/LoginActivity.java
+++ app/src/main/java/com/foi/air1712/instad/accountManagement/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.foi.air1712.instad.accountManagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.foi.air1712.instad.PrikazSvihActivity;
 import com.foi.air1712.instad.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +17,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+    @OnClick(R.id.skip_button)
+    public void skipButtonClicked(View view){
+        startActivity(new Intent(LoginActivity.this, PrikazSvihActivity.class));
     }
 }
