9ad8ea2a28de860fa96f50c8f699f36a66a7f5f1
hranicd <dhranic2@foi.hr>
2017-12-09 00:31:36 +0100

    enable editing user name add new or update existing user display name show user email address

--- app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazPostavkeFragment.java
+++ app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazPostavkeFragment.java
@@ -26,6 +26,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
+import java.util.Objects;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -35,8 +37,16 @@ import butterknife.OnClick;
  */
 
 public class PrikazPostavkeFragment extends Fragment {
+    @BindView(R.id.progressBarUserSettings)
+    ProgressBar pb;
+    Button editNameBtn;
+    EditText nameEditText;
+    EditText userEmail;
     View view;
     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    Activity act;
+    private boolean editDNameActive = false;
+
     public static PrikazPostavkeFragment newInstance() {
         PrikazPostavkeFragment fragment = new PrikazPostavkeFragment();
         return fragment;
@@ -45,12 +55,23 @@ public class PrikazPostavkeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_postavke, container, false);
+        act = getActivity();
+        pb = view.findViewById(R.id.progressBarUserSettings);
+        //show user email address
+        final FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        nameEditText = (EditText) view.findViewById(R.id.user_diplay_name);
+        if(currentUser.getDisplayName()!=null){
+            nameEditText.setText(currentUser.getDisplayName());
+        }
+        userEmail = (EditText) view.findViewById(R.id.user_mail_info);
+        userEmail.setText(currentUser.getEmail());
         //user logout
         Button button = (Button) view.findViewById(R.id.logout_button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -68,6 +89,51 @@ public class PrikazPostavkeFragment extends Fragment {
 
             }
         });
+        //edit user Display Name
+        editNameBtn = (Button) view.findViewById(R.id.edit_name_btn);
+
+        editNameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //nameEditText = (EditText) view.findViewById(R.id.user_diplay_name);
+                String ime = nameEditText.getText().toString();
+                Log.i("ime:", ime);
+                if (!editDNameActive) {
+                    nameEditText.setEnabled(true);
+                    editNameBtn.setText("Save");
+                    editDNameActive = true;
+                }else{
+
+                    pb.setVisibility(View.VISIBLE);
+                    if(!Objects.equals(ime, "")){
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(ime).build();
+                        firebaseAuth.getCurrentUser().updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                pb.setVisibility(View.INVISIBLE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(act,"Updated!", Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(act,"Update failed!", Toast.LENGTH_SHORT).show();
+                                    nameEditText.setText(firebaseAuth.getCurrentUser().getDisplayName());
+                                }
+                            }
+                        });
+                    }else{
+                        Toast.makeText(getActivity(),"Saving failed! Empty field", Toast.LENGTH_SHORT).show();
+                        if(currentUser.getDisplayName()!=null)
+                        nameEditText.setText(currentUser.getDisplayName());
+                    }
+
+                    nameEditText.setEnabled(false);
+                    editNameBtn.setText("Edit");
+                    editDNameActive = false;
+                }
+
+            }
+        });
         return view;
     }
+
 }
