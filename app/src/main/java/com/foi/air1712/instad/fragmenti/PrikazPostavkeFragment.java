6559fb68c0fc8a215c157cb207a4230c183d8edf
hranicd <dhranic2@foi.hr>
2017-12-09 13:18:10 +0100

    user email change enable edit and save new email to database

--- app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazPostavkeFragment.java
+++ app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazPostavkeFragment.java
@@ -54,6 +54,8 @@ public class PrikazPostavkeFragment extends Fragment {
     private EditText editConfirmPass;
     private Button changePassBtn;
     private boolean editPasswordActive = false;
+    private Button editEmailBtn;
+    private boolean editEmailActive = false;
 
     public static PrikazPostavkeFragment newInstance() {
         PrikazPostavkeFragment fragment = new PrikazPostavkeFragment();
@@ -136,6 +138,43 @@ public class PrikazPostavkeFragment extends Fragment {
                     nameEditText.setEnabled(false);
                     editNameBtn.setText("Edit");
                     editDNameActive = false;
+                }
+
+            }
+        });
+        //edit user email address
+        editEmailBtn = (Button) view.findViewById(R.id.edit_mail_btn);
+        editEmailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = userEmail.getText().toString();
+                if (!editEmailActive) {
+                    userEmail.setEnabled(true);
+                    editEmailBtn.setText("Save");
+                    editEmailActive = true;
+                }else{
+                    pb.setVisibility(View.VISIBLE);
+                    if(!Objects.equals(email, "")){
+                        currentUser.updateEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                pb.setVisibility(View.INVISIBLE);
+                                if(!task.isSuccessful()){
+                                    Toast.makeText(getActivity(),"Saving failed! "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(getActivity(),"User email updated!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }else{
+                        pb.setVisibility(View.INVISIBLE);
+                        Toast.makeText(getActivity(),"Saving failed! Empty field!", Toast.LENGTH_SHORT).show();
+                        userEmail.setText(currentUser.getEmail());
+                    }
+
+                    userEmail.setEnabled(false);
+                    editEmailBtn.setText("Edit");
+                    editEmailActive = false;
                 }
 
             }
