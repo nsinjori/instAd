4c2138fd855f14037adc8735af06527bc8f7d97a
nsinjori <nsinjori@foi.hr>
2017-12-09 19:09:59 +0100

    Prikaz na mapi omogućen prikaz događaja na mapi kod detaljnog prikaza informacija

--- app/src/main/java/com/foi/air1712/instad/fragmenti/DetaljniPrikazFragment.java
+++ app/src/main/java/com/foi/air1712/instad/fragmenti/DetaljniPrikazFragment.java
@@ -7,11 +7,19 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.foi.air1712.database.Dogadaji;
 import com.foi.air1712.instad.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
 
 import java.text.ParseException;
@@ -24,7 +32,7 @@ import java.util.Date;
  */
 
 
-public class DetaljniPrikazFragment extends Fragment{
+public class DetaljniPrikazFragment extends Fragment implements OnMapReadyCallback {
 
     private ImageView dogadajSlika;
     private TextView dogadajNaziv;
@@ -32,6 +40,10 @@ public class DetaljniPrikazFragment extends Fragment{
     private TextView dogadajDatumP;
     private TextView dogadajDatumK;
     private Dogadaji dogadajDohvacen = null;
+
+    private FrameLayout mapaPrikaz;
+    private com.google.android.gms.maps.MapFragment mapFragment;
+    private GoogleMap map = null;
 
     @Nullable
     @Override
@@ -56,6 +68,16 @@ public class DetaljniPrikazFragment extends Fragment{
         dogadajDatumP.setText("Početak: " + lijepiDatum(dogadajDohvacen.getDatum_pocetka()));
         dogadajDatumK.setText("Kraj: " + lijepiDatum(dogadajDohvacen.getDatum_kraj()));
 
+        mapaPrikaz=(FrameLayout)rootView.findViewById(R.id.mapa_prikaz);
+        //dodavanje google karte u layout
+
+        mapFragment = new com.google.android.gms.maps.MapFragment();
+
+        mapFragment.getMapAsync(this);
+
+        getActivity().getFragmentManager().beginTransaction().add(R.id.mapa_prikaz, mapFragment).commit();
+
+        mapaPrikaz.setFocusable(true);
 
 
         return rootView;
@@ -81,9 +103,24 @@ public class DetaljniPrikazFragment extends Fragment{
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
         return formatiraniDatum;
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        double dogadajLatitude = Double.parseDouble(dogadajDohvacen.getLatitude());
+        double dogadajLongitude = Double.parseDouble(dogadajDohvacen.getLongitude());
+        LatLng dogadajKoordinate = new LatLng(dogadajLatitude, dogadajLongitude);
+
+        Marker marker=map.addMarker(new MarkerOptions()
+                .title(dogadajDohvacen.getObjekt())
+                //.snippet("Nekaj")
+                .position(dogadajKoordinate));
+        marker.showInfoWindow();
+        //Pozicionira i zumirana lokaciju od koordinata
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(dogadajKoordinate).zoom(15).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
 }
