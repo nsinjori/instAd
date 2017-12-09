1aa63863917903fac13430f88824bec7dee01df0
hranicd <dhranic2@foi.hr>
2017-12-09 21:12:53 +0100

    show all locations popunjava se recyclerView s podacima o svim lokacijama sva mjesta su označena kao favoriti jer nema još provjere

---
+++ app/src/main/java/com/foi/air1712/instad/PrikazLokacijaAdapter.java
@@ -0,0 +1,69 @@
+package com.foi.air1712.instad;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.foi.air1712.database.Lokacije;
+
+import java.util.List;
+
+/**
+ * Created by Darko on 9.12.2017..
+ */
+
+public class PrikazLokacijaAdapter extends RecyclerView.Adapter<PrikazLokacijaAdapter.LokacijaViewHolder>{
+    private final List<Lokacije> lokacije;
+    private final Context context;
+
+    public PrikazLokacijaAdapter(List<Lokacije> lokacije, Context context) {
+        this.lokacije = lokacije;
+        this.context = context;
+    }
+
+    public static class LokacijaViewHolder extends RecyclerView.ViewHolder{
+        CardView cv;
+        TextView naziv;
+        TextView adresa;
+        ImageView slika;
+
+        public LokacijaViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cv_lokacija);
+            naziv = (TextView)itemView.findViewById(R.id.cv_lokacija_naziv);
+            adresa = (TextView)itemView.findViewById(R.id.cv_adresa_lokacije);
+            slika = (ImageView)itemView.findViewById(R.id.cv_lokacija_slika);
+        }
+    }
+    @Override
+    public LokacijaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.lokacija,parent,false);
+        LokacijaViewHolder lokacijaViewHolder = new LokacijaViewHolder(v);
+        return lokacijaViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final LokacijaViewHolder holder, final int position) {
+        String nazivLokacije = lokacije.get(position).getNaziv();
+        String adresaLokacije = lokacije.get(position).getAdresa();
+        holder.naziv.setText(nazivLokacije);
+        holder.adresa.setText(adresaLokacije);
+        //ovisno o tome jel prati ili ne se bude mjenjala slika, sad tak
+        holder.slika.setImageResource(R.drawable.heart_full);
+    }
+
+    @Override
+    public int getItemCount() {
+        return lokacije.size();
+    }
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+}
--- app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazFavoritiFragment.java
+++ app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazFavoritiFragment.java
@@ -2,17 +2,34 @@ package com.foi.air1712.instad.fragmenti;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.foi.air1712.core.DataLoadedListener;
+import com.foi.air1712.core.DataLoader;
+import com.foi.air1712.database.Dogadaji;
+import com.foi.air1712.database.Lokacije;
+import com.foi.air1712.instad.PrikazLokacijaAdapter;
 import com.foi.air1712.instad.R;
+import com.foi.air1712.instad.loaders.DbDataLoader;
+import com.foi.air1712.instad.loaders.WsDataLoader;
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+import java.util.ArrayList;
 
 /**
  * Created by Nikola on 7.12.2017..
  */
 
-public class PrikazFavoritiFragment extends Fragment {
+public class PrikazFavoritiFragment extends Fragment implements DataLoadedListener{
+    private RecyclerView rv;
+    private ArrayList<Lokacije> listaLokacija;
+
     public static PrikazFavoritiFragment newInstance() {
         PrikazFavoritiFragment fragment = new PrikazFavoritiFragment();
         return fragment;
@@ -26,6 +43,35 @@ public class PrikazFavoritiFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favoriti, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_favoriti, container, false);
+
+        FlowManager.init(new FlowConfig.Builder(this.getContext()).build());
+
+        rv = (RecyclerView)rootView.findViewById(R.id.rv_lokacije);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+        rv.setLayoutManager(layoutManager);
+        rv.setHasFixedSize(true);
+        DataLoader dataLoader;
+        if(Lokacije.getAll().isEmpty()){
+
+            dataLoader = new WsDataLoader();
+        }else{
+
+            dataLoader = new DbDataLoader();
+        }
+        dataLoader.loadData(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onDataLoaded(ArrayList<Dogadaji> dogadaji, ArrayList<Lokacije> lokacije) {
+        listaLokacija = lokacije;
+        initializeAdapter();
+    }
+
+    private void initializeAdapter() {
+        PrikazLokacijaAdapter adapter= new PrikazLokacijaAdapter(listaLokacija, getContext());
+        rv.setAdapter(adapter);
     }
 }
