3f4095c9a4016e4a1cc2b269917a692ad3b77d3a
nsinjori <nsinjori@foi.hr>
2018-01-18 18:26:05 +0100

    Osvježavanje - prijavljen osvježavanje podataka kod uloge prijavljenog korisnika

--- app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazSvihFragment.java
+++ app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazSvihFragment.java
@@ -2,6 +2,7 @@ package com.foi.air1712.instad.fragmenti;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -19,9 +20,16 @@ import com.foi.air1712.instad.loaders.DbDataLoader;
 import com.foi.air1712.instad.loaders.WsDataLoader;
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
 
 import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by Nikola on 7.12.2017..
@@ -30,6 +38,7 @@ import java.util.ArrayList;
 
 public class PrikazSvihFragment extends Fragment implements DataLoadedListener {
 
+    private SwipeRefreshLayout swipeContainer;
     private RecyclerView rv;
     private ArrayList<Dogadaji> DohvaceniDogadaji;
     private ArrayList<Lokacije> dohvaceneLokacije;
@@ -52,6 +61,16 @@ public class PrikazSvihFragment extends Fragment implements DataLoadedListener {
         View rootView =  inflater.inflate(R.layout.fragment_prikaz_svih, container, false);
 
         FlowManager.init(new FlowConfig.Builder(this.getContext()).build());
+
+        //za pull to refresh
+        swipeContainer = (SwipeRefreshLayout) rootView.findViewById(R.id.swipeContainer);
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                obrisiDohvati();
+            }
+        });
 
         //za risajklera
         rv=(RecyclerView)rootView.findViewById(R.id.rv_fragment);
@@ -88,11 +107,55 @@ public class PrikazSvihFragment extends Fragment implements DataLoadedListener {
         initializeAdapter();
 
         System.out.println("Dohvacene lokacije z baze/firebase --> " + dohvaceneLokacije.size());
+        swipeContainer.setRefreshing(false);
 
     }
 
     private void initializeAdapter(){
-        PrikazSvihAdapter adapter = new PrikazSvihAdapter(DohvaceniDogadaji, getContext());
+        ArrayList<Dogadaji> azuriraniDogadaji = new ArrayList<Dogadaji>();
+
+        for(Dogadaji dogadaj : DohvaceniDogadaji){
+            /** proba za datume da ih ne uzima ak su stari**/
+            String dtStart = dogadaj.getDatum_kraj();
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = new Date();
+            try {
+                date = format.parse(dtStart);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            Date danas = new Date();
+
+            if(!date.before(danas)){
+                azuriraniDogadaji.add(dogadaj);
+                System.out.println("918 - " + dogadaj);
+            }
+            /** kraj provjere datum **/
+        }
+
+        /*
+        Set<Dogadaji> hs = new HashSet<>();
+        hs.addAll(azuriraniDogadaji);
+        azuriraniDogadaji.clear();
+        azuriraniDogadaji.addAll(hs);
+        */
+
+        PrikazSvihAdapter adapter = new PrikazSvihAdapter(azuriraniDogadaji, getContext());
         rv.setAdapter(adapter);
+    }
+
+    public void obrisiDohvati(){
+        Dogadaji.deleteAllDogadaji();
+        DohvaceniDogadaji.clear();
+        rv.setAdapter(null);
+        //Toast.makeText(getActivity(),"Briseeem", Toast.LENGTH_SHORT).show();
+        rv.removeAllViewsInLayout();
+        DataLoader dataLoader;
+        dataLoader = new WsDataLoader();
+        dataLoader.loadData(this);
+
+
+        //swipeContainer.setRefreshing(false);
+
     }
 }
