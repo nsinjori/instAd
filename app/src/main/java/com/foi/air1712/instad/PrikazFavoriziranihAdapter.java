4087166747ae76a0c5614613e18cff50cc1fb47c
hranicd <dhranic2@foi.hr>
2018-01-19 17:38:44 +0100

    prikaz događaja u novom fragmentu prikaz testnog događaa na lokaciji foi, dodan adapter za fragment favoriziranih

---
+++ app/src/main/java/com/foi/air1712/instad/PrikazFavoriziranihAdapter.java
@@ -0,0 +1,86 @@
+package com.foi.air1712.instad;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.foi.air1712.database.Dogadaji;
+import com.foi.air1712.instad.fragmenti.DetaljniPrikazFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Darko on 11.1.2018..
+ */
+
+public class PrikazFavoriziranihAdapter extends RecyclerView.Adapter<PrikazFavoriziranihAdapter.DogadajViewHolder>{
+    List<Dogadaji> dogadaji;
+    Context context;
+
+    public static class DogadajViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView objekt;
+        TextView naziv;
+        ImageView slika;
+
+        DogadajViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cv);
+            objekt = (TextView)itemView.findViewById(R.id.objektcv);
+            naziv = (TextView)itemView.findViewById(R.id.nazivcv);
+            slika = (ImageView)itemView.findViewById(R.id.slikacv);
+        }
+    }
+    public PrikazFavoriziranihAdapter(List<Dogadaji> dogadaji, Context context){
+        this.dogadaji = dogadaji;
+        this.context = context;
+    }
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public PrikazFavoriziranihAdapter.DogadajViewHolder onCreateViewHolder(ViewGroup viewGroup, final int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.dogadaj, viewGroup, false);
+        PrikazFavoriziranihAdapter.DogadajViewHolder dvh = new PrikazFavoriziranihAdapter.DogadajViewHolder(v);
+        return dvh;
+    }
+    @Override
+    public void onBindViewHolder(PrikazFavoriziranihAdapter.DogadajViewHolder dogadajViewHolder, final int i) {
+        String dtStart = dogadaji.get(i).getDatum_pocetka();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat izlFormat = new SimpleDateFormat("EEEE, d MMM yyyy");
+        Date date = new Date();
+        String formatiraniDatum = "";
+        try {
+            date = format.parse(dtStart);
+            formatiraniDatum = izlFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        dogadajViewHolder.objekt.setText(dogadaji.get(i).getObjekt() + "\n" + formatiraniDatum);
+        dogadajViewHolder.naziv.setText(dogadaji.get(i).getNaziv());
+        final Context context = dogadajViewHolder.slika.getContext();
+        Picasso.with(context).load(dogadaji.get(i).getSlika()).into(dogadajViewHolder.slika);
+    }
+    @Override
+    public int getItemCount() {
+        return dogadaji.size();
+    }
+}
--- app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazFavoriziranihFragment.java
+++ app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazFavoriziranihFragment.java
@@ -1,14 +1,82 @@
 package com.foi.air1712.instad.fragmenti;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.foi.air1712.core.DataLoadedListener;
+import com.foi.air1712.core.DataLoader;
+import com.foi.air1712.database.Dogadaji;
+import com.foi.air1712.database.Lokacije;
+import com.foi.air1712.instad.PrikazFavoriziranihAdapter;
+import com.foi.air1712.instad.R;
+import com.foi.air1712.instad.loaders.DbDataLoader;
+import com.foi.air1712.instad.loaders.WsDataLoader;
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowManager;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Darko on 10.1.2018..
  */
 
-public class PrikazFavoriziranihFragment extends Fragment{
+public class PrikazFavoriziranihFragment extends Fragment implements DataLoadedListener{
+    private RecyclerView rv;
+    private ArrayList<Dogadaji> DohvaceniDogadaji;
+
     public static Fragment newInstance() {
         PrikazFavoriziranihFragment prikazFavoriziranihFragment = new PrikazFavoriziranihFragment();
         return prikazFavoriziranihFragment;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView =  inflater.inflate(R.layout.fragment_favorizirani, container, false);
+        FlowManager.init(new FlowConfig.Builder(this.getContext()).build());
+        rv=(RecyclerView)rootView.findViewById(R.id.rv_favorizirani);
+        rv.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        rv.setHasFixedSize(true);
+        DataLoader dataLoader;
+        if(Dogadaji.getAll().isEmpty()){
+            dataLoader = new WsDataLoader();
+        } else { dataLoader = new DbDataLoader();
+        }
+        dataLoader.loadData(this);
+        return rootView;
+    }
+
+    @Override
+    public void onDataLoaded(ArrayList<Dogadaji> dogadaji, ArrayList<Lokacije> lokacije) {
+        DohvaceniDogadaji = dogadaji;
+        initializeAdapter();
+    }
+    private void initializeAdapter(){
+        ArrayList<Dogadaji> azuriraniDogadaji = new ArrayList<Dogadaji>();
+        for(Dogadaji dogadaj : DohvaceniDogadaji){
+            String dtStart = dogadaj.getDatum_kraj();
+            Date date = new Date();
+            try {
+                date = new SimpleDateFormat("yyyy-MM-dd").parse(dtStart);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            if(!date.before(new Date())&&dogadaj.getObjekt().equalsIgnoreCase("foi")){
+                azuriraniDogadaji.add(dogadaj);
+            }
+        }
+        PrikazFavoriziranihAdapter adapter = new PrikazFavoriziranihAdapter(azuriraniDogadaji, getContext());
+        rv.setAdapter(adapter);
+    }
+
 }
