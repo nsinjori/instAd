52ff2755d872e12f5cdb431fa3aab001989330f4
hranicd <dhranic2@foi.hr>
2018-01-19 17:45:01 +0100

    prikaz događaja prema favoritima korisnika dohvaćanej podataka o favoriziranim lokacijama korisnika iz baze i filtriranje događaja na temelju dobivenih podataka

--- app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazFavoriziranihFragment.java
+++ app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazFavoriziranihFragment.java
@@ -15,12 +15,19 @@ import com.foi.air1712.instad.PrikazFavoriziranihAdapter;
 import com.foi.air1712.instad.R;
 import com.foi.air1712.instad.loaders.DbDataLoader;
 import com.foi.air1712.instad.loaders.WsDataLoader;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by Darko on 10.1.2018..
@@ -29,6 +36,7 @@ import java.util.Date;
 public class PrikazFavoriziranihFragment extends Fragment implements DataLoadedListener{
     private RecyclerView rv;
     private ArrayList<Dogadaji> DohvaceniDogadaji;
+    private ArrayList<String> favoriti=new ArrayList<>();
 
     public static Fragment newInstance() {
         PrikazFavoriziranihFragment prikazFavoriziranihFragment = new PrikazFavoriziranihFragment();
@@ -59,7 +67,7 @@ public class PrikazFavoriziranihFragment extends Fragment implements DataLoadedL
     @Override
     public void onDataLoaded(ArrayList<Dogadaji> dogadaji, ArrayList<Lokacije> lokacije) {
         DohvaceniDogadaji = dogadaji;
-        initializeAdapter();
+        ucitajPodatke();
     }
     private void initializeAdapter(){
         ArrayList<Dogadaji> azuriraniDogadaji = new ArrayList<Dogadaji>();
@@ -71,12 +79,38 @@ public class PrikazFavoriziranihFragment extends Fragment implements DataLoadedL
             } catch (ParseException e) {
                 e.printStackTrace();
             }
-            if(!date.before(new Date())&&dogadaj.getObjekt().equalsIgnoreCase("foi")){
+            if(!date.before(new Date())&&favoriti.contains(dogadaj.getObjekt())){
                 azuriraniDogadaji.add(dogadaj);
             }
         }
         PrikazFavoriziranihAdapter adapter = new PrikazFavoriziranihAdapter(azuriraniDogadaji, getContext());
         rv.setAdapter(adapter);
     }
-
+    void ucitajPodatke() {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        DatabaseReference ref = database.getReference("users").child(firebaseAuth.getCurrentUser().getUid()).child("favs");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if(snapshot!=null) {
+                    Map<String, String> mapData = (Map) snapshot.getValue();
+                    if(mapData!=null) {
+                        for (Map.Entry<String, String> entry : mapData.entrySet()) {
+                            String fav = entry.getKey().toString();
+                            if (!favoriti.contains(fav))
+                                favoriti.add(fav);
+                        }
+                    }
+                    initializeAdapter();
+                }else{
+                    System.out.println("Nema favorita!");
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getMessage());
+            }
+        });
+    }
 }
