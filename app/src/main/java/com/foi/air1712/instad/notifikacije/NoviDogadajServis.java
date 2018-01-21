7f4aea9bf12e977cfe2cfca4b0f251dd94786ad8
hranicd <dhranic2@foi.hr>
2018-01-21 14:33:48 +0100

    dodan servis kreiran servis u kojem ce se implementirati listener za promatranje promjena u bazi za nove dogadaje

---
+++ app/src/main/java/com/foi/air1712/instad/notifikacije/NoviDogadajServis.java
@@ -0,0 +1,18 @@
+package com.foi.air1712.instad.notifikacije;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Darko on 19.1.2018..
+ */
+
+public class NoviDogadajServis extends Service{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
