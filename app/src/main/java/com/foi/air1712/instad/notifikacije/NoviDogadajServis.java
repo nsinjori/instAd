719d3372701735d18ab26ac789a40189e6a46782
hranicd <dhranic2@foi.hr>
2018-01-21 15:19:32 +0100

    prikaz notifikacije toast poruka prilikom dodavanja događaja zamijenjena s notifikacijom

--- app/src/main/java/com/foi/air1712/instad/notifikacije/NoviDogadajServis.java
+++ app/src/main/java/com/foi/air1712/instad/notifikacije/NoviDogadajServis.java
@@ -1,12 +1,15 @@
 package com.foi.air1712.instad.notifikacije;
 
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import com.foi.air1712.database.Dogadaji;
+import com.foi.air1712.instad.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,6 +27,7 @@ public class NoviDogadajServis extends Service{
     private static List<String> trenutni = new ArrayList<>();
     private DatabaseReference ref;
     private ValueEventListener vel;
+    private int id = 001;
 
     @Nullable
     @Override
@@ -56,6 +60,14 @@ public class NoviDogadajServis extends Service{
     }
 
     private void notificiraj(Dogadaji dogadaj) {
-        Toast.makeText(getBaseContext(),"Događaj: "+dogadaj.getNaziv(), Toast.LENGTH_SHORT).show();
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(getBaseContext())
+                        .setSmallIcon(R.drawable.cast_ic_notification_small_icon)
+                        .setContentTitle("Novi događaj")
+                        .setContentText(dogadaj.getNaziv())
+                        .setAutoCancel(true);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(id, mBuilder.build());
+        id++;
     }
 }
