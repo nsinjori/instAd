3fa90917e2a90c9e81482f1a793079e13ba58fc7
hranicd <dhranic2@foi.hr>
2018-01-22 11:37:35 +0100

    kreiranje testnog geofenca prilikom dolaska na tab postavke kreira se testna geofence lokacija

--- app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazPostavkeFragment.java
+++ app/src/main/java/com/foi/air1712/instad/fragmenti/PrikazPostavkeFragment.java
@@ -1,12 +1,18 @@
 package com.foi.air1712.instad.fragmenti;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,11 +23,17 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.foi.air1712.database.Dogadaji;
 import com.foi.air1712.instad.MainActivity;
 import com.foi.air1712.instad.R;
 import com.foi.air1712.instad.accountManagement.LoginActivity;
+import com.foi.air1712.instad.notifikacije.GeoFenceLocationServis;
 import com.foi.air1712.instad.notifikacije.NoviDogadajServis;
 import com.google.android.gms.internal.zzaap;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -42,6 +54,7 @@ import butterknife.OnClick;
  */
 
 public class PrikazPostavkeFragment extends Fragment {
+    private static final float GEOFENCE_RADIUS = 25;
     @BindView(R.id.progressBarUserSettings)
     ProgressBar pb;
     Button editNameBtn;
@@ -60,6 +73,7 @@ public class PrikazPostavkeFragment extends Fragment {
     private boolean editEmailActive = false;
     private Button notificiranje;
     private boolean startaniServis;
+    private GeofencingClient geofencingClient;
 
     public static PrikazPostavkeFragment newInstance() {
         PrikazPostavkeFragment fragment = new PrikazPostavkeFragment();
@@ -80,6 +94,8 @@ public class PrikazPostavkeFragment extends Fragment {
         act = getActivity();
         pb = view.findViewById(R.id.progressBarUserSettings);
         //show user email address
+        geofencingClient = LocationServices.getGeofencingClient(getActivity());
+        postaviGeofence();
         final FirebaseUser currentUser = firebaseAuth.getCurrentUser();
         nameEditText = (EditText) view.findViewById(R.id.user_diplay_name);
         if(currentUser.getDisplayName()!=null){
@@ -270,6 +286,19 @@ public class PrikazPostavkeFragment extends Fragment {
         });
         return view;
     }
+
+    private void postaviGeofence() {
+        if(isLocationAccessPermitted()){
+            Log.i("GEOFENCE", "falta permisiona");
+            ActivityCompat.requestPermissions(act, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }else{
+            removeLocationAlert();
+            Log.i("GEOFENCE", "dodavanje");
+            addLocationAlert(46.30764,16.33857,"Foijo");
+
+        }
+    }
+
     private void provjeraServisa() {
         if(isMyServiceRunning(NoviDogadajServis.class)){
             startaniServis = true;
@@ -287,5 +316,66 @@ public class PrikazPostavkeFragment extends Fragment {
             }
         }
         return false;
+    }
+    @SuppressLint("MissingPermission")
+    public void addLocationAlert(double lat, double lng, final String key){
+        //String key = ""+lat+"-"+lng;
+        Geofence geofence = getGeofence(lat, lng, key);
+        geofencingClient.addGeofences(getGeofencingRequest(geofence),
+                getGeofencePendingIntent())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            //Toast.makeText(act, "Location alter has been added" + key, Toast.LENGTH_SHORT).show();
+                        }else{
+                            //Toast.makeText(act, "Location alter could not be added", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    private void removeLocationAlert(){
+        geofencingClient.removeGeofences(getGeofencePendingIntent())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            //Toast.makeText(act, "Location alters have been removed", Toast.LENGTH_SHORT).show();
+                        }else{
+                           // Toast.makeText(act, "Location alters could not be removed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    private PendingIntent getGeofencePendingIntent() {
+        Intent intent = new Intent(act, GeoFenceLocationServis.class);
+        return PendingIntent.getService(act, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+    private GeofencingRequest getGeofencingRequest(Geofence geofence) {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_DWELL);
+        builder.addGeofence(geofence);
+        return builder.build();
+    }
+
+    private Geofence getGeofence(double lat, double lang, String key) {
+        return new Geofence.Builder()
+                .setRequestId(key)
+                .setCircularRegion(lat, lang, GEOFENCE_RADIUS)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                        Geofence.GEOFENCE_TRANSITION_DWELL)
+                .setLoiteringDelay(1000)
+                .build();
+    }
+    private boolean isLocationAccessPermitted(){
+        if (ContextCompat.checkSelfPermission(act,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }else{
+            return false;
+        }
     }
 }
